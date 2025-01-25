e8139df956a051ee87bd9050679b78638a0d47de
JovanKatanic <jovan.katanic0101@gmail.com>
2025-01-25 08:21:16 +0100

    played around with executing sudo commands

--- execute.zig
+++ execute.zig
@@ -1,1 +1,9 @@
 const std = @import("std");
+pub fn main() !void {
+    var cmd = std.process.Child.init(&[_][]const u8{ "sudo", "ls" }, std.heap.page_allocator);
+    cmd.stdout_behavior = .Inherit;
+    cmd.stdin_behavior = .Inherit;
+
+    try cmd.spawn();
+    _ = try cmd.wait();
+}
