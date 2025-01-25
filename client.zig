ad35fdfe5944ab09e87b28ff810732113bdff5a6
JovanKatanic <jovan.katanic0101@gmail.com>
2025-01-25 06:34:33 +0100

    fixed client server to use pure sockets

--- client.zig
+++ client.zig
@@ -1,32 +1,31 @@
 const std = @import("std");
 const BUFFER_SIZE = 4096;
-const SERVER_ADDR = "http://127.0.0.1:8080";
+const HOST_ADDR = "127.0.0.1";
+const PORT = 8080;
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var client = std.http.Client{ .allocator = allocator };
-    defer client.deinit();
-    var buf: [BUFFER_SIZE]u8 = undefined;
-    const uri = try std.Uri.parse(SERVER_ADDR);
-    var request = try client.open(.POST, uri, .{
-        .server_header_buffer = &buf,
-    });
-    defer request.deinit();
-    const message = "hej";
-    request.transfer_encoding = .{ .content_length = message.len };
-    try request.send();
-    _ = try request.writer().write(message);
-    try request.finish();
-    try request.wait();
-
-    var chunk_buf: [BUFFER_SIZE]u8 = undefined;
+    const address = try std.net.Address.parseIp4(HOST_ADDR, PORT);
+    const conn = try std.net.tcpConnectToAddress(address);
+    defer conn.close();
+    const recv_buf = try allocator.alloc(u8, BUFFER_SIZE);
     while (true) {
-        const chunk_size = try request.reader().read(&chunk_buf);
-        if (chunk_size == 0) {
-            break;
+        const command = try std.io.getStdIn().reader().readUntilDelimiterOrEofAlloc(allocator, '\n', BUFFER_SIZE) orelse "";
+        defer allocator.free(command);
+
+        if (command.len == 0) {
+            continue;
         }
-        try std.io.getStdOut().writer().writeAll(chunk_buf[0..chunk_size]);
+        try conn.writer().writeAll(command);
+        while (true) {
+            const bytes_read = try conn.reader().read(recv_buf);
+            std.debug.print("Server: {d}\n", .{bytes_read});
+            if (bytes_read == 0 or bytes_read != BUFFER_SIZE) {
+                std.debug.print("Break read loop\n", .{});
+                break;
+            }
+        }
     }
 
     std.debug.print("EXITED CLIENT\n", .{});
--- server.zig
+++ server.zig
@@ -1,28 +1,35 @@
 const std = @import("std");
 const HOST_ADDR = "127.0.0.1";
 const PORT = 8080;
+const BUFFER_SIZE = 4096;
 pub fn main() !void {
     const address = try std.net.Address.parseIp4(HOST_ADDR, PORT);
-
     var server = try address.listen(std.net.Address.ListenOptions{});
     defer server.deinit();
 
     while (true) {
         const conn = try server.accept();
-        _ = try std.Thread.spawn(.{}, handleConnection, .{conn});
+        _ = try std.Thread.spawn(.{}, handleConnection, .{conn.stream});
     }
 }
 
-fn handleConnection(conn: std.net.Server.Connection) !void {
-    defer conn.stream.close();
-    var buffer: [1024]u8 = undefined;
-    var http_server = std.http.Server.init(conn, &buffer);
-    var req = try http_server.receiveHead();
-    const body_len = req.head.content_length orelse 0;
-    std.debug.print("Received user data: {s}\n", .{buffer[req.head_end .. req.head_end + body_len]});
+fn handleConnection(conn: std.net.Stream) !void {
+    defer conn.close();
+    var recv_buf: [BUFFER_SIZE]u8 = undefined;
 
-    const response_body = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n" ** 100;
+    const response_body = "a" ** 200;
 
-    try req.respond(response_body, std.http.Server.Request.RespondOptions{ .transfer_encoding = .chunked });
+    while (true) {
+        const bytes_read = try conn.reader().read(&recv_buf);
+        if (bytes_read == 0) {
+            std.debug.print("Break loop\n", .{});
+            break;
+        }
+        //TODO execute command
+
+        std.debug.print("Server: {s}\n", .{recv_buf[0..bytes_read]});
+        try conn.writer().writeAll(response_body);
+    }
+
     std.debug.print("EXITED SERVER THREAD", .{});
 }
